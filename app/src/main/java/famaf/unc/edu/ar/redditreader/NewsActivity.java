cb753fe9349bd622bb6ba86993dcdfd7831ca5f5
Pablo Ventura <pablogventura@gmail.com>
2016-09-15 19:35:58 -0300

    listo el layout

--- app/src/main/java/famaf/unc/edu/ar/redditreader/NewsActivity.java
+++ app/src/main/java/famaf/unc/edu/ar/redditreader/NewsActivity.java
@@ -30,10 +30,11 @@ public class NewsActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_sign_in) {
-            NewsActivityFragment newsfragment = (NewsActivityFragment)
+            /*NewsActivityFragment newsfragment = (NewsActivityFragment)
                     getSupportFragmentManager().findFragmentById(R.id.news_activity_fragment_id);
             TextView textView = (TextView) findViewById(R.id.loginStatusTextView);
             textView.setText("User XXXX logged in");
+            */
             return true;
         }
 
